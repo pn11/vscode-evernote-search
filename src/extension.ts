2488eeb618e9261483afae4a12d96bc561ce70eb
oka <pn11@users.noreply.github.com>
2021-05-14 05:29:35 +0900

    set publisher info, fix logging

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,32 @@ import * as vscode from 'vscode';
 import { Options, PythonShell } from 'python-shell';
 
 export function activate(context: vscode.ExtensionContext) {
+	let log = vscode.window.createOutputChannel("Evernote Search");
+
 	let disposable = vscode.commands.registerCommand('evernote-search.search', () => {
 		// get Extension Path
-		let ext_path = vscode.extensions.getExtension('undefined_publisher.evernote-search')?.extensionPath;
+		let ext_path = vscode.extensions.getExtension('pn11.evernote-search')?.extensionPath;
+		if (ext_path != undefined){
+			log.appendLine("Extension path: " + ext_path);
+		}
+		else {
+			log.appendLine("Error: Extension evernote-search not found.");
+			log.show();
+			vscode.window.showErrorMessage("Extension evernote-search not found.");
+			return;
+		}
+
 		// get pythonpath from VSCode config
 		let pythonpath = vscode.workspace.getConfiguration('python').get<string>('pythonPath');
-		console.log("Used Python Env:", pythonpath);
+		if (pythonpath != undefined){
+			log.appendLine("Using Python Env: " + pythonpath);
+		}
+		else {
+			log.appendLine("Error: Python environment not set.");
+			log.show();
+			vscode.window.showErrorMessage("Python environment not set.");
+			return;
+		}
 		
 		let promptOptions = {
 		    prompt: 'Evernote search query',
@@ -17,10 +37,11 @@ export function activate(context: vscode.ExtensionContext) {
 		};
 		vscode.window.showInputBox(promptOptions).then(val => {
 		    if (val != undefined) {
-			  search(val, pythonpath, ext_path)
+			  search(val, pythonpath, ext_path, log);
 		    }
 			else {
-				console.log('error');
+				log.appendLine('Unexpected error when calling Python.');
+				log.show();
 			}
 		});
 		
@@ -30,7 +51,7 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // Request to Evernote using Python API
-function search(search_query: string, pythonpath: string | undefined, ext_path: string | undefined) {
+function search(search_query: string, pythonpath: string | undefined, ext_path: string | undefined, log: vscode.OutputChannel) {
 	let options: Options = {
 		mode: 'text',
 		pythonPath: pythonpath,
@@ -38,15 +59,15 @@ function search(search_query: string, pythonpath: string | undefined, ext_path: 
 		scriptPath: ext_path,
 		args: [search_query]
 	};
-
 	PythonShell.run('python/search.py', options, function (err, res) {
 		if (err) {
-			console.log(err);
+			log.appendLine((res || ['']).join('\n'));
+			log.show();
 			vscode.window.showErrorMessage((res || ['']).join('\n'));
 			throw err;
 		}
 		const res_str: string = (res || ['']).join('\n');
-		console.log(res_str);
+		log.appendLine(res_str)
 		const panel = vscode.window.createWebviewPanel('searchResult', 'Evernote Search Result', vscode.ViewColumn.One);
 		panel.webview.html = res_str;
 	});
