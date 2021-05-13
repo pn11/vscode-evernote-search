5e2d65799071bc44a416d6ed1db72c4def02bf94
oka <pn11@users.noreply.github.com>
2021-05-14 00:56:22 +0900

    input search query using input box

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,6 @@
 import * as vscode from 'vscode';
 import { Options, PythonShell } from 'python-shell';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('evernote-search.search', () => {
 		// get Extension Path
@@ -10,31 +8,48 @@ export function activate(context: vscode.ExtensionContext) {
 		// get pythonpath from VSCode config
 		let pythonpath = vscode.workspace.getConfiguration('python').get<string>('pythonPath');
 		console.log("Used Python Env:", pythonpath);
-
-		let options: Options = {
-			mode: 'text',
-			pythonPath: pythonpath,
-			pythonOptions: ['-u'], // get print results in real-time
-			scriptPath: ext_path,
-			args: ['テスト']
+		
+		let promptOptions = {
+		    prompt: 'Evernote search query',
+		    placeHolder: 'Evernote search query: https://dev.evernote.com/doc/articles/search_grammar.php',
+		    value: '',
+		    ignoreFocusOut: true
 		};
-
-		PythonShell.run('python/search.py', options, function (err, res) {
-			console.log('Test executing a python script');
-			if (err) {
-				console.log(err);
-				vscode.window.showErrorMessage((res || ['']).join('\n'));
-				throw err;
+		vscode.window.showInputBox(promptOptions).then(val => {
+		    if (val != undefined) {
+			  search(val, pythonpath, ext_path)
+		    }
+			else {
+				console.log('error');
 			}
-			const res_str: string = (res || ['']).join('\n');
-			console.log(res_str);
-			const panel = vscode.window.createWebviewPanel('searchResult', 'Evernote Search Result', vscode.ViewColumn.One);
-			panel.webview.html = res_str;
 		});
-	});
+		
 
+	});
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+// Request to Evernote using Python API
+function search(search_query: string, pythonpath: string | undefined, ext_path: string | undefined) {
+	let options: Options = {
+		mode: 'text',
+		pythonPath: pythonpath,
+		pythonOptions: ['-u'], // get print results in real-time
+		scriptPath: ext_path,
+		args: [search_query]
+	};
+
+	PythonShell.run('python/search.py', options, function (err, res) {
+		if (err) {
+			console.log(err);
+			vscode.window.showErrorMessage((res || ['']).join('\n'));
+			throw err;
+		}
+		const res_str: string = (res || ['']).join('\n');
+		console.log(res_str);
+		const panel = vscode.window.createWebviewPanel('searchResult', 'Evernote Search Result', vscode.ViewColumn.One);
+		panel.webview.html = res_str;
+	});
+}
+
 export function deactivate() {}
