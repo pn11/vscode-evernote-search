24c4d7cc8459eab487063ff11bf222720b92e4f2
oka <pn11@users.noreply.github.com>
2021-08-01 18:01:08 +0900

    remove Python code and rewrite with TypeScript

--- src/extension.ts
+++ src/extension.ts
@@ -1,88 +1,108 @@
 import * as vscode from 'vscode';
 import * as fs from "fs";
-import { Options, PythonShell } from 'python-shell';
+import * as ini from "ini"
+import * as evernote from "evernote"
 
 export function activate(context: vscode.ExtensionContext) {
 	let log = vscode.window.createOutputChannel("Evernote Search");
 
 	let disposable = vscode.commands.registerCommand('evernote-search.search', () => {
-		// get Extension Path
-		let ext_path = vscode.extensions.getExtension('pn11.evernote-search')?.extensionPath;
-		if (ext_path != undefined){
-			log.appendLine("Extension path: " + ext_path);
-		}
-		else {
-			log.appendLine("Error: Extension evernote-search not found.");
-			log.show();
-			vscode.window.showErrorMessage("Extension evernote-search not found.");
-			return;
-		}
-
-		// get pythonpath from VSCode config
-		let pythonpath = vscode.workspace.getConfiguration('python').get<string>('pythonPath');
-		if (pythonpath != undefined){
-			log.appendLine("Using Python Env: " + pythonpath);
-		}
-		else {
-			log.appendLine("Error: Python environment not set.");
-			log.show();
-			vscode.window.showErrorMessage("Python environment not set.");
-			return;
-		}
-		/*
-		if (fs.existsSync(pythonpath)) {
-		} else {
-			log.appendLine("Error: Python environment not found.");
-			log.show();
-			vscode.window.showErrorMessage("Error: Python environment not found.");
-			return;
-		}
-		*/
-		
 		let promptOptions = {
-		    prompt: 'Evernote search query',
-		    placeHolder: 'Evernote search query: https://dev.evernote.com/doc/articles/search_grammar.php',
-		    value: '',
-		    ignoreFocusOut: true
+			prompt: 'Evernote search query',
+			placeHolder: 'Evernote search query: https://dev.evernote.com/doc/articles/search_grammar.php',
+			value: '',
+			ignoreFocusOut: true
 		};
 		vscode.window.showInputBox(promptOptions).then(val => {
-		    if (val != undefined) {
-			  search(val, (pythonpath || ''), (ext_path || ''), log);
-		    }
+			if (val != undefined) {
+				search(val, log);
+			}
 			else {
-				log.appendLine('Unexpected error when calling Python.');
+				log.appendLine('Unexpected error.');
 				log.show();
 			}
 		});
-		
 
 	});
 	context.subscriptions.push(disposable);
 }
 
-// Request to Evernote using Python API
-export function search(search_query: string, pythonpath: string, ext_path: string, log: vscode.OutputChannel) {
-	let options: Options = {
-		mode: 'text',
-		pythonPath: pythonpath,
-		pythonOptions: ['-u'], // get print results in real-time
-		scriptPath: ext_path,
-		args: [search_query]
-	};
-	PythonShell.run('python/search.py', options, function (err, res) {
-		if (err) {
-			log.appendLine((err.message));
-			log.appendLine((err.traceback.toString()));
-			log.appendLine((res || ['']).join('\n'));
-			log.show();
-			vscode.window.showErrorMessage((res || ['']).join('\n'));
-			throw err;
-		}
-		const res_str: string = (res || ['']).join('\n');
-		log.appendLine(res_str)
-		const panel = vscode.window.createWebviewPanel('searchResult', 'Evernote Search Result', vscode.ViewColumn.One);
-		panel.webview.html = res_str;
-	});
+function load_token(log: vscode.OutputChannel): string {
+	const homedir = require('os').homedir();
+	try {
+		const config = ini.parse(fs.readFileSync(homedir + '/.evernote-search.cfg', 'utf-8'))
+		const token = config['settings']['EVERNOTE_TOKEN']
+		return token
+	} catch (e) {
+		log.appendLine(e)
+		log.show()
+		return ''
+	}
 }
 
-export function deactivate() {}
+function request_search(search_query: string, log: vscode.OutputChannel) {
+	const token = load_token(log)
+	if (token == '') {
+		return
+	}
+	var client = new evernote.Client({
+		token: token,
+		sandbox: false, // change to false when you are ready to switch to production
+		china: false, // change to true if you wish to connect to YXBJ - most of you won't
+	});
+
+	var noteStore = client.getNoteStore();
+	var filter = new evernote.NoteStore.NoteFilter({
+		// この記法が使える https://dev.evernote.com/doc/articles/search_grammar.php
+		words: search_query,
+		ascending: false
+	})
+	var spec = new evernote.NoteStore.NotesMetadataResultSpec({
+		includeTitle: true,
+		includeContentLength: true,
+		includeCreated: true,
+		includeUpdated: true,
+		includeDeleted: true,
+		includeUpdateSequenceNum: true,
+		includeNotebookGuid: true,
+		includeTagGuids: true,
+		includeAttributes: true,
+		includeLargestResourceMime: true,
+		includeLargestResourceSize: true,
+	})
+	return Promise.all([
+		client.getUserStore().getUser(),
+		noteStore.findNotesMetadata(filter, 0, 100, spec)])
+		.then(function (res) {
+			const [user, notesMetadataList] = res
+			return createHTML(user, notesMetadataList)
+		}).catch(function (err) {
+			log.appendLine('Error code = ' + err.errorCode + ': ' + err.parameter)
+			log.show()
+			return 'Search Failed. See error message.'
+		})
+}
+
+function make_link(user: evernote.Types.User, note: any) {
+	return 'https://www.evernote.com/shard/' + user.shardId + '/nl/' + user.id + '/ ' + note.guid
+}
+
+
+function createHTML(user: evernote.Types.User, notesMetadataList: evernote.NoteStore.NotesMetadataList) {
+	var html = ''
+	html += '<ul>'
+	notesMetadataList.notes?.filter(note => {
+		html += '<li><a href=' + make_link(user, note) + '>' + note.title + '</a></li>'
+
+	}
+	)
+	html += '</ul>'
+	return html
+}
+
+export function search(search_query: string, log: vscode.OutputChannel) {
+	const panel = vscode.window.createWebviewPanel('searchResult', 'Evernote Search Result', vscode.ViewColumn.One);
+	request_search(search_query, log)?.then((html) => { panel.webview.html = html })
+}
+
+export function deactivate() { }
